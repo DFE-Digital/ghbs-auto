039cc95e9ba66f126a4a92baf3569d8076540dd1
TomPaine-DfEAuto <tom.paine@education.gov.uk>
2025-08-12 10:29:30 +0100

    Revert "Initial commit of a working Ruby, Selenium, Cucumber pack."

--- features/step_definitions/fabs_home_page_steps.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../pages/fabs_home_page_methods'
-
-def fabs_homepage
-  @home_page ||= FabsHomePage.new(@driver, SECRETS)
-end
-
-Given('I search for {string} in to the main search on the fabs homepage') do |search_term|
-  fabs_homepage.run_main_search(search_term)
-end
