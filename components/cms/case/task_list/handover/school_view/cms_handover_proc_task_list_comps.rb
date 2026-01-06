132900d7fd0930331832c309af7d5ac045dfb79d
Tom Paine <tom.paine@airelogic.com>
2026-01-06 09:48:45 +0000

    ES-983 - Added school facing views comps for handover screens.

---
+++ components/cms/case/task_list/handover/school_view/cms_handover_proc_task_list_comps.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class CmsHandoverProcTaskListComps
+  include Capybara::DSL
+
+  def text_page_heading
+    find(:xpath, "//h1")
+  end
+
+  def text_case_number_and_school_name
+    find(:xpath, "//h1/preceding-sibling::span")
+  end
+
+  def link_download_link(document_name)
+    find(:xpath, "//dl/div/dd/a[text()='#{document_name}']")
+  end
+
+  def radio_downloaded_docs_yes
+    find(:xpath, "//label[@for='handover_packs_has_downloaded_documents_true']")
+  end
+
+  def radio_downloaded_docs_no
+    find(:xpath, "//label[@for='handover_packs_has_downloaded_documents_false']")
+  end
+
+  def button_cancel
+    find(:xpath, "//a[text()='Cancel']")
+  end
+
+  def button_continue
+    find(:xpath, "//input[@value='Continue']")
+  end
+end
